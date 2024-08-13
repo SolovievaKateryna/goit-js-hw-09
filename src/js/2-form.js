dce4d24d5d80bddb441a34eaab28286d32d8b4cf
SolovievaKateryna <deliriymtremens@gmail.com>
2024-08-13 21:22:50 +0300

    correct warning message

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,11 +1,4 @@
-import iziToast from 'izitoast';
-
-import 'izitoast/dist/css/iziToast.min.css';
-
 const form = document.querySelector('.feedback-form');
-
-form.addEventListener('input', onInput);
-form.addEventListener('submit', onSubmit);
 
 const formData = JSON.parse(localStorage.getItem('feedback-form-state')) || {
     email: '',
@@ -23,31 +16,25 @@ function onInput(event) {
     localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
 
-function onSubmit(event) {
+form.addEventListener('input', event => {
+    const { name, value } = event.target;
+    formData[name] = value.trim();
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', event => {
     event.preventDefault();
-
-    const { email, message } = event.currentTarget.elements;
-
-    const trimmedEmail = email.value.trim();
-    /*const trimmedMessage = message.value.trim();*/
-
-    if (trimmedEmail === '' || trimmedMessage === '') {
-        iziToast.error({
-            position: 'topRight',
-            message: 'Fill please all fields',
-            timeout: 5000,
-        });
-    return;
+    if (!formData.email || !formData.message) {
+        alert('Fill please all fields');
+        return;
     }
 
     console.log(formData);
 
-    localStorage.removeItem('feedback-form-state');
-    event.currentTarget.reset();
-
-    iziToast.success({
-    position: 'topRight',
-    message: 'Data sent!',
-    timeout: 2500,
-    });
-}
+    localStorage.removeItem(STORAGE_KEY);
+    formData.email = '';
+    formData.message = '';
+    emailInput.value = '';
+    messageTextarea.value = '';
+    form.reset();
+});
